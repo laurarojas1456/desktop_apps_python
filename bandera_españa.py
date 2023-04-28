ca88c60eb38a414e7f88f4e6104cedb55665f6e1
sistemas <you@example.com>
2023-04-28 07:28:36 -0500

    Se creó el diagrama de flujo y se agregó al README

---
+++ bandera_españa.py
@@ -0,0 +1,23 @@
+#-------------------------------------
+# frama rojo
+#-------------------------------------
+from tkinter import Frame
+
+
+frame_rojo = Frame("ventana_principal")
+frame_rojo.config(bg="red", width=300, height=600)
+frame_rojo.place(x=0, y=0)
+
+#-------------------------------------
+# frama amarillo
+#-------------------------------------
+frame_amarillo = Frame("ventana_principal")
+frame_amarillo.config(bg="yellow", width=300, height=600)
+frame_amarillo.place(x=200, y=0)
+
+#-------------------------------------
+# frama rojo
+#-------------------------------------
+frame_rojo = Frame("ventana_principal")
+frame_rojo.config(bg="red", width=300, height=600)
+frame_rojo.place(x=400, y=0)
---
+++ bandera_noruega.py
@@ -0,0 +1,55 @@
+#-------------------------
+# Desktop apps No. 1
+#-------------------------
+
+# se importa la libreria tkinter con todas sus funciones 
+from tkinter import *
+
+#-------------------------
+# funciones de la app
+#-------------------------
+
+#-------------------------
+# ventana  principal de la app
+#--------------------------
+
+# se declara una variable llamada ventana_principal, que adquiere las caracteristicas de un objetivo Tk()
+ventana_principal = Tk()
+
+# titulo de  ventana
+ventana_principal.title("Especialidad en Sistemas-Guanentá")
+
+#tamaño de la ventana 
+ventana_principal.geometry("500x500")
+
+# deshabiltar boton de maximizar 
+ventana_principal.resizable(False, False)
+
+# color de fondo de la ventana 
+ventana_principal.config(bg="red")
+
+#------------------------------
+# frame azul
+#-----------------------------
+frame_azul = Frame(ventana_principal)
+frame_azul.config(bg="blue", width=500, height=250)
+frame_azul.place(x=0, y=0)
+
+#------------------------------
+# frame azul
+#-----------------------------
+frame_azul= Frame(ventana_principal)
+frame_azul.config(bg="blue", width=500, height=125)
+frame_azul.place(x=0, y=250)
+
+#------------------------------
+# frame rojo
+#-----------------------------
+frame_rojo= Frame(ventana_principal)
+frame_rojo.config(bg="red", width=500, height=125)
+frame_rojo.place(x=0, y=375)
+
+# run
+# # se el metodo mainloop()de la clase Tk(a través de la instancia ventana_principal.Este metodo despliega la ventana en pantalla y queda a la espera de lo que el usuario haga (click en un boton, escribir, etc). Cada acción del usuario se conoce un evento. El método mainloop() es un bucle infinito.
+
+ventana_principal.mainloop()
